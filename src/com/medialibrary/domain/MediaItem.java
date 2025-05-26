e74f1fd936804e6905bd911fd11d3f8c3729aef8
Lamont <viniciusmessias52@gmail.com>
2025-05-26 08:45:19 -0300

    feat: initialize MediaItem class structure

--- src/com/medialibrary/domain/MediaItem.java
+++ src/com/medialibrary/domain/MediaItem.java
@@ -1,5 +1,25 @@
 package com.medialibrary.domain;
 
 public class MediaItem {
-    private String name;
+    private final String title;
+    private final int yearPublication;
+
+    public MediaItem(String title, int yearPublication) {
+        this.title = title;
+        this.yearPublication = yearPublication;
+    }
+
+    @Override
+    public String toString() {
+        return "Media [" + "Title: " + this.title + ", Year: " + this.yearPublication + "]";
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public int getYearPublication() {
+        return this.yearPublication;
+    }
+
 }
