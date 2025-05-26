442453525b5b9ed7e9edd1245a403b661edff961
Lamont <viniciusmessias52@gmail.com>
2025-05-26 11:01:02 -0300

    feat: implement Movie class extending MediaItem

---
+++ src/com/medialibrary/domain/Movie.java
@@ -0,0 +1,30 @@
+package com.medialibrary.domain;
+
+public class Movie extends MediaItem {
+    private final String director;
+    private final int durationInMinutes;
+
+
+    public Movie(String title, int yearPublication, String director, int durationInMinutes) {
+        super(title, yearPublication);
+        this.director = director;
+        this.durationInMinutes = durationInMinutes;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie [Title: " + getTitle() +
+                ", Year: " + getYearPublication() +
+                ", Director: " + this.director +
+                ", Duration: " + this.durationInMinutes +
+                " min]";
+    }
+
+    public String getDirector() {
+        return this.director;
+    }
+
+    public int getDurationInMinutes() {
+        return this.durationInMinutes;
+    }
+}
