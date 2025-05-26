e4019d4812a55c6e6bad2503a27e02f417d6d0ba
Lamont <viniciusmessias52@gmail.com>
2025-05-26 11:38:55 -0300

    feat: show polymorphism with Library holding Book and Movie objects

--- src/com/medialibrary/app/Main.java
+++ src/com/medialibrary/app/Main.java
@@ -1,10 +1,18 @@
 package com.medialibrary.app;
 
+import com.medialibrary.domain.Book;
+import com.medialibrary.domain.Library;
 import com.medialibrary.domain.Movie;
 
 public class Main {
     public static void main(String[] args) {
-        Movie testing = new Movie("Se7en", 1995, "David Fincher", 127);
-        System.out.println(testing);
+        Library library69 = new Library();
+        Book book1 = new Book("The Godfather", 1969, "9788055111360", "G. P. Putnam's Sons");
+        Movie movie1 = new Movie("Se7en", 1995, "David Fincher", 127);
+
+        library69.addMediaItem(book1);
+        library69.addMediaItem(movie1);
+
+        library69.showMedias();
     }
 }
