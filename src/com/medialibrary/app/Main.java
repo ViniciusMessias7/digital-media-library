9b92255fbf76eabf3b992b97d222eca60f71a426
Lamont <viniciusmessias52@gmail.com>
2025-05-27 08:51:40 -0300

    test: add a test in Main class about new method findMediaItemByTittle in Libray class

--- src/com/medialibrary/app/Main.java
+++ src/com/medialibrary/app/Main.java
@@ -2,6 +2,7 @@ package com.medialibrary.app;
 
 import com.medialibrary.domain.Book;
 import com.medialibrary.domain.Library;
+import com.medialibrary.domain.MediaItem;
 import com.medialibrary.domain.Movie;
 
 public class Main {
@@ -12,7 +13,13 @@ public class Main {
 
         library69.addMediaItem(book1);
         library69.addMediaItem(movie1);
-
         library69.showMedias();
+
+        MediaItem foundItem = library69.findMediaItemByTitle("Se7en");
+        if (foundItem != null) {
+            System.out.println("Found " + foundItem);
+        } else {
+            System.out.println("Media item not found.");
+        }
     }
 }
