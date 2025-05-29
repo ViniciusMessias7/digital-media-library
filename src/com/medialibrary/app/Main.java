a531de9570e513fba79e96f1e1ed822fce06835c
Lamont <viniciusmessias52@gmail.com>
2025-05-29 11:28:25 -0300

    refactoring: separate interaction in Main class by user type

--- src/com/medialibrary/app/Main.java
+++ src/com/medialibrary/app/Main.java
@@ -9,39 +9,91 @@ public class Main {
         Scanner input = new Scanner(System.in);
         Library library = new Library();
         char command = ' ';
+        char userChoiceCommand = ' ';
 
-        System.out.println("### Media Library v1.0 ###");
-        while (command != '0') {
-            System.out.println("1. Add new media item");
-            System.out.println("2. List all medias item");
-            System.out.println("3. Find media item by title");
-            System.out.println("4. Remove media item by title");
+        System.out.println("Welcome to Digital Media Library!");
+        while (userChoiceCommand != '0') {
+            System.out.println("1. Employee");
+            System.out.println("2. Member");
             System.out.println("0. Exit");
             System.out.print("> ");
-            String userInput = input.nextLine();
-            if (userInput.length() != 1) {
-                System.out.println("\nInvalid command, please try again:\n");
+            String userChoiceStr = input.nextLine();
+
+            if (!userChoiceStr.equals("0") && !userChoiceStr.equals("1") && !userChoiceStr.equals("2")) {
+                System.out.println("Invalid command, please try again (Or 0 to exit): ");
+                System.out.println();
                 continue;
             }
-            command = userInput.charAt(0);
+            userChoiceCommand = userChoiceStr.charAt(0);
+
+            if (userChoiceCommand == '1' || userChoiceCommand == '2') {
+                break;
+            }
+        }
+
+        if (userChoiceCommand == '0') {
+            System.out.println("Exiting application. Goodbye!");
+            return;
+        }
+
+        if (userChoiceCommand == '1') {
+            System.out.println("\n-- Librarian Session Starting --");
+            handleLibrarianSession(input, library);
+        } else {
+            System.out.println("\n-- Member Session Starting --");
+            handleMemberSession(input,library);
+        }
+
+    }
+
+    private static String promptAndReadTitle(Scanner input) {
+        System.out.print("Title: ");
+        return input.nextLine();
+    }
+
+    private static int promptAndReadYear(Scanner input) {
+        System.out.print("Year of publication: ");
+        int yearOfPublication = input.nextInt();
+        input.nextLine();
+        return yearOfPublication;
+    }
+
+    private static void handleLibrarianSession(Scanner input, Library library) {
+        char command = ' ';
+        while (command != '0') {
+            System.out.println("1. Add new media item");
+            System.out.println("2. Remove media item by title");
+            System.out.println("0. Exit");
+            System.out.print("> ");
+            String userChoiceStr = input.nextLine();
+            if (!userChoiceStr.equals("0") && !userChoiceStr.equals("1") && !userChoiceStr.equals("2")) {
+                System.out.println("Invalid command, please try again (Or 0 to exit): ");
+                System.out.println();
+                continue;
+            }
+            command = userChoiceStr.charAt(0);
 
             switch (command) {
                 case '1':
                     char instruction = ' ';
+
                     System.out.println("\n--- Add New Media Item ---");
                     while (instruction != '0') {
                         System.out.println("What type of media would you like to add?");
                         System.out.println("1. Book");
                         System.out.println("2. Movie");
                         System.out.println("3. Music Album");
-                        System.out.println("0. Exit and return to main menu");
+                        System.out.println("0. Exit and return to menu");
                         System.out.print("> ");
                         String typeChoiceStr = input.nextLine();
-                        if (typeChoiceStr.length() != 1) {
-                            System.out.println("\nInvalid command, please try again:\n");
+
+                        if (!typeChoiceStr.equals("0") && !typeChoiceStr.equals("1") && !typeChoiceStr.equals("2") && !typeChoiceStr.equals("3")) {
+                            System.out.println("Invalid command, please try again (Or 0 to exit): ");
+                            System.out.println();
                             continue;
                         }
                         instruction = typeChoiceStr.charAt(0);
+
                         switch (instruction) {
                             case '1':
                                 System.out.println("\nLet's add a book.");
@@ -60,6 +112,7 @@ public class Main {
                                 System.out.println("Book added successfully!");
                                 library.addMediaItem(book);
                                 instruction = '0';
+                                System.out.println();
                                 break;
                             case '2':
                                 System.out.println("\nLet's add a movie.");
@@ -79,6 +132,7 @@ public class Main {
                                 System.out.println("movie added successfully!");
                                 library.addMediaItem(movie);
                                 instruction = '0';
+                                System.out.println();
                                 break;
                             case '3':
                                 System.out.println("\nLet's add a music album.");
@@ -98,29 +152,16 @@ public class Main {
                                 System.out.println("Music album added successfully!");
                                 library.addMediaItem(musicAlbum);
                                 instruction = '0';
+                                System.out.println();
                                 break;
-                            default:
-                                System.out.println("\nInvalid option, please try again.\n");
+                            case '0':
+                                System.out.println("Addition canceled");
+                                System.out.println();
                                 break;
                         }
                     }
-                    System.out.println();
                     break;
                 case '2':
-                    library.showMedias();
-                    System.out.println();
-                    break;
-                case '3':
-                    System.out.print("Title: ");
-                    MediaItem foundItem = library.findMediaItemByTitle(input.nextLine());
-                    if (foundItem == null) {
-                        System.out.println("Media item not found.");
-                    } else {
-                        System.out.println("Found " + foundItem);
-                    }
-                    System.out.println();
-                    break;
-                case '4':
                     System.out.print("Title: ");
                     MediaItem removeItem = library.removeMediaItemByTitle(input.nextLine());
                     if (removeItem == null) {
@@ -131,26 +172,47 @@ public class Main {
                     System.out.println();
                     break;
                 case '0':
-                    System.out.println("Exiting Media Library. Goodbye!");
+                    System.out.println("Exiting session Librarian. Goodbye!");
                     break;
-                default:
-                    System.out.println("\nInvalid option, please try again.\n");
+            }
+        }
+
+    }
+
+    private static void handleMemberSession(Scanner input, Library library) {
+        char command = ' ';
+        while (command != '0') {
+            System.out.println("1. List all medias item");
+            System.out.println("2. Find media item by title");
+            System.out.println("0. Exit");
+            System.out.print("> ");
+            String userChoiceStr = input.nextLine();
+            if (!userChoiceStr.equals("0") && !userChoiceStr.equals("1") && !userChoiceStr.equals("2")) {
+                System.out.println("Invalid command, please try again (Or 0 to exit): ");
+                System.out.println();
+                continue;
+            }
+            command = userChoiceStr.charAt(0);
+            switch (command) {
+                case '1':
+                    library.showMedias();
+                    System.out.println();
+                    break;
+                case '2':
+                    System.out.print("Title: ");
+                    MediaItem foundItem = library.findMediaItemByTitle(input.nextLine());
+                    if (foundItem == null) {
+                        System.out.println("Media item not found.");
+                    } else {
+                        System.out.println("Found " + foundItem);
+                    }
+                    System.out.println();
+                    break;
+                case '0':
+                    System.out.println("Exiting session member. Goodbye!");
                     break;
             }
 
         }
     }
-
-    private static String promptAndReadTitle(Scanner input) {
-        System.out.print("Title: ");
-        return input.nextLine();
-    }
-
-    private static int promptAndReadYear(Scanner input) {
-        System.out.print("Year of publication: ");
-        int yearOfPublication = input.nextInt();
-        input.nextLine();
-        return yearOfPublication;
-    }
-
 }
